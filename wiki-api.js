e63da6ea570c7ff7d5bfe83ab8b2ce67ae54586b
101175 <160013569+101175@users.noreply.github.com>
2025-05-27 13:46:20 +0000

    Test wiki Api 1

---
+++ wiki-api.js
@@ -0,0 +1,10 @@
+import wiki from 'wikijs';
+// const wiki = require('wikijs').default;
+
+wiki({ apiUrl: 'https://en.wikipedia.org/w/api.php' })
+	.page('Habsburg')
+	.then(page => page.summary())
+	.then(summary => console.log(summary))
+	.then(page => page.info())
+	.then(info => console.log(info))
+	.then(console.log);
